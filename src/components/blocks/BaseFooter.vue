0d5ac4140d3959e4555736f5c4a4ecd05917b631
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-06-17 12:43:57 +0200

    bottom-footer

--- src/components/blocks/BaseFooter.vue
+++ src/components/blocks/BaseFooter.vue
@@ -1,13 +1,15 @@
 <template>
   <footer>
     <SectionTopFooter />
+    <SectionBottomFooter />
   </footer>
 </template>
 
 <script>
+import SectionBottomFooter from "../sections/SectionBottomFooter.vue"
 import SectionTopFooter from "../sections/SectionTopFooter.vue"
 export default {
-  components: { SectionTopFooter },
+  components: { SectionTopFooter, SectionBottomFooter },
 }
 </script>
 
---
+++ src/components/sections/SectionBottomFooter.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container">
+    <section class="bottom-footer">
+      <div class="inner-container">
+        <div class="row">
+          <div class="col-9">
+            <p>
+              © Copyright 2012 - 2020 | Avada Theme by Theme Fusion | All Rights
+              Reserved | Powered by WordPress
+            </p>
+          </div>
+          <div class="col-3">
+            <div class="social-container">
+              <div class="socials d-flex">
+                <div class="facebook social-icon">
+                  <i class="fa-brands fa-facebook-f"></i>
+                </div>
+                <div class="twitter social-icon">
+                  <i class="fa-brands fa-twitter"></i>
+                </div>
+                <div class="ig social-icon">
+                  <i class="fa-brands fa-instagram"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.bottom-footer {
+  background-color: #000;
+  padding: 2rem 0;
+  & p {
+    font-size: 0.7rem;
+    margin: 0;
+  }
+  .social-icon {
+    background-color: #ee6059;
+    width: 1.875rem;
+    height: 1.875rem;
+    padding: 0.625rem;
+    margin-right: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+  }
+}
+</style>
